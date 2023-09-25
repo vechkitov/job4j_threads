6bb2b0f42215c88c90c827b3f13d0555e817aab4
Sergey <vechkitov@bk.ru>
2023-09-25 21:40:39 +0200

    3. Immutable объекты [#267918] - исходное состояние

---
+++ src/main/java/ru/job4j/linked/Node.java
@@ -0,0 +1,22 @@
+package ru.job4j.linked;
+
+public class Node<T> {
+    private Node<T> next;
+    private T value;
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+}
